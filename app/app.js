7130b20cdf207e9db7d0282d87ca192f5b0f43bf
Pedro Dias <phds@cin.ufpe.br>
2016-10-18 11:24:43 -0300

    removed passportjs from app

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var passport     = require('passport');
 
 var routes = require('./routes/index');
 var api    = require('./routes/api');
@@ -24,8 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', routes);
 app.use('/api', api);

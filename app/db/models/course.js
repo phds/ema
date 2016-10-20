b1a9134e4613a568b5ca5001d171566559809f7b
Pedro Dias <phds@cin.ufpe.br>
2016-10-20 11:08:04 -0300

    added code to course

--- app/db/models/course.js
+++ app/db/models/course.js
@@ -4,13 +4,17 @@ module.exports = function(sequelize, DataTypes) {
     name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
         models.course.belongsToMany(models.user, {through: 'studentCourse'});
-        models.course.belongsToMany(models.question, {through: 'questionCourse'})
+        models.course.belongsToMany(models.question, {through: 'questionCourse'});
         models.course.belongsTo(models.user, {as: 'professor'});
         models.course.hasOne(models.answer);
         models.course.hasOne(models.question);
